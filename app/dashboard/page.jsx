b7f101c654ae5749cb18036972f311e424f4fa8e
selman <gunduzgece546@gmail.com>
2023-12-24 10:55:39 +0300

    cards transactions

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import styles from '../ui/dashboard/dashboard.module.css'
+import Card from '../ui/dashboard/card/cards'
+import Rightbar from '../ui/dashboard/rightbar/rightbar'
+import Transactions from '../ui/dashboard/transaction/transaction'
+import Chart from '../ui/dashboard/chart/chart'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.sidebar}>
+        <Rightbar />
+      </div>
+    </div>
   )
 }
 
